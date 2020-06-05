ca2f72e2b457dfbad1d58303cdcbc8a3f19f49c9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-05 15:16:56 +0100

    Add header to home page

--- controller.rb
+++ controller.rb
@@ -7,5 +7,6 @@ require_relative('./models/transaction')
 also_reload('./models/*')
 
 get '/xpences' do
+    @user = { 'first_name' => 'Anna', 'last_name' => 'Smith', 'age' => 21}
     erb( :home )
 end
