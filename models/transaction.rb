f3776a003e8e5f217da85ace543b3f57ca19cc61
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-05 12:24:40 +0100

    Add save method for Transaction

--- models/transaction.rb
+++ models/transaction.rb
@@ -11,4 +11,15 @@ class Transaction
         @tag_id = options['tag_id'].to_i
     end
 
+    def save()
+        sql = "INSERT INTO transactions
+        ( amount, merchant_id, tag_id )
+        VALUES
+        ( $1, $2, $3 )
+        RETURNING id"
+        values = [@amount, @merchant_id, @tag_id]
+        id = SqlRunner.run(sql, values)[0]['id']
+        @id = id.to_i
+    end
+
 end
