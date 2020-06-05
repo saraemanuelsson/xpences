ddb854821da50333344e0e7b19e9339e78f34e28
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-05 13:21:18 +0100

    Add delete method for transaction

--- models/transaction.rb
+++ models/transaction.rb
@@ -32,4 +32,10 @@ class Transaction
         SqlRunner.run(sql, values)
     end
 
+    def delete()
+        sql = "DELETE FROM transactions WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
 end
