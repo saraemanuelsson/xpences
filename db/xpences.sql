bfb334b065d1c547fa931f97c2bb181d5c1e3e02
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-05 11:03:18 +0100

    Created tables for merchants, tags and transactions in sql file

---
+++ db/xpences.sql
@@ -0,0 +1,23 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+DROP TABLE merchants;
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    active BIT
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(255),
+    colour VARCHAR(255),
+    active BIT
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    amount REAL,
+    merchant_id INT REFERENCES merchants(id) ON DELETE SET NULL,
+    tag_id INT REFERENCES tags(id) ON DELETE SET NULL
+);
