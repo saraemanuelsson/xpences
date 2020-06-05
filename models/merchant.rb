6fb27d3741ad17322f4b2ac0e843a22759f4f1d4
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-05 12:32:44 +0100

    Add delete method for Merchant

--- models/merchant.rb
+++ models/merchant.rb
@@ -32,4 +32,10 @@ class Merchant
         SqlRunner.run(sql, values)
     end
 
+    def delete()
+        sql = "DELETE FROM merchants WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
 end
