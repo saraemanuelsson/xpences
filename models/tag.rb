b5f65b234ede5e8974b20335de48f57d2f8d19f9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-05 12:20:52 +0100

    Add save method for Tag

--- models/tag.rb
+++ models/tag.rb
@@ -11,4 +11,16 @@ class Tag
         @active = options['active']
         @id = options['id'].to_i if options['id']
     end
+
+    def save()
+        sql = "INSERT INTO tags
+        ( category, colour, active )
+        VALUES 
+        ( $1, $2, $3 )
+        RETURNING id"
+        values = [@category, @colour, @active]
+        id = SqlRunner.run(sql, values)[0]['id']
+        @id = id.to_i
+    end
+    
 end
