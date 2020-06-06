cc230515a885d9981bef440cff9d12a845a17609
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-06 10:59:48 +0100

    Improved routes

---
+++ app.rb
@@ -0,0 +1,10 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/transactions_controller')
+require_relative('controllers/merchants_controller')
+require_relative('controllers/tags_controller')
+
+get '/' do
+    @user = { 'first_name' => 'Anna', 'last_name' => 'Smith', 'age' => 21}
+    erb( :home )
+end
--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -1,30 +1,31 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
-require_relative('./models/merchant')
-require_relative('./models/tag')
-require_relative('./models/transaction')
-also_reload('./models/*')
+require_relative('../models/merchant.rb')
+require_relative('../models/tag.rb')
+require_relative('../models/transaction.rb')
+also_reload('../models/*')
 require('pry')
 
-get '/xpences' do
-    @user = { 'first_name' => 'Anna', 'last_name' => 'Smith', 'age' => 21}
-    erb( :home )
-end
-
-get '/xpences/add-expense' do
+get '/add-expense' do
     @merchants = Merchant.all()
     @tags = Tag.all()
     erb( :new_expense )
 end
 
-get '/xpences/expenses' do
+get '/expenses' do
     @transactions = Transaction.all()
     erb( :expenses )
 end
 
-post '/xpences' do
+post '/expenses' do
     expense = Transaction.new(params)
     expense.save()
-    redirect( :expenses )
+    redirect( '/expenses' )
 end
+
+get '/expenses/:id/edit-expense' do
+    @transaction = Transaction.find_by_id(params[:id])
+    erb( :edit_transaction)
+end
+
