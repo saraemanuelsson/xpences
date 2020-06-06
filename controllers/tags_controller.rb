093f022176b8e4a2dd1f2951f08e41e06cee396e
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-06 20:31:24 +0100

    Set up routes in tags_controller

---
+++ controllers/tags_controller.rb
@@ -0,0 +1,34 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+require_relative('../models/merchant.rb')
+require_relative('../models/tag.rb')
+require_relative('../models/transaction.rb')
+also_reload('../models/*')
+require('pry')
+
+get '/tags' do
+    @Tags = Tag.all()
+    erb( :"tags/index" )
+end
+
+get '/tags/new' do
+    erb( :"tags/new" )
+end
+
+post '/tags' do
+    tag = Tag.new(params)
+    tag.save()
+    redirect( '/tags' )
+end
+
+get '/tags/:id/edit' do
+    @tag = Tag.find_by_id(params[:id])
+    erb( :"tags/edit" )
+end
+
+post '/tags/:id' do
+    tag_to_update = Tag.new(params)
+    tag_to_update.update()
+    redirect( '/tags' )
+end
