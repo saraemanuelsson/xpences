772190538a62ea2c354f79905a83e29f8b24011b
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-10 10:14:40 +0100

    Added styling to deactivated tags and merchants in index so they stand out from the active ones

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require_relative('controllers/expenses_controller')
 require_relative('controllers/merchants_controller')
 require_relative('controllers/tags_controller')
 require_relative('controllers/user_controller')
+require_relative('controllers/budget_controller')
 
 get '/' do
     @user = { 'first_name' => 'Anna', 'last_name' => 'Smith', 'age' => 21}
